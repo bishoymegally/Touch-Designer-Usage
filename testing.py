3fd266e2f78af66360090b224b39560b4c6a1b23
bishoymegally <bishoymegally.ok@gmail.com>
2026-08-14 23:45:21 -0500

    Added a line that goes from pointer to thumb on each hand

--- testing.py
+++ testing.py
@@ -1,10 +1,11 @@
 import mediapipe as mp
 import cv2
 import time
+import math
 
 
 base_options = mp.tasks.BaseOptions(model_asset_path="hand_landmarker.task")
-options = mp.tasks.vision.HandLandmarkerOptions(base_options = base_options)
+options = mp.tasks.vision.HandLandmarkerOptions(base_options = base_options, num_hands = 2)
 handlandmarker = mp.tasks.vision.HandLandmarker.create_from_options(options)
 
 
@@ -24,7 +25,7 @@ start_time = time.perf_counter()
 
 while True:
     ret, frame = capture.read()
-
+    frame = cv2.flip(frame, 1)
     if not ret:
         break
 
@@ -40,16 +41,37 @@ while True:
 
     result = handlandmarker.detect(mp_image)
 
-    hand = result.hand_landmarks[0] if len(result.hand_landmarks) != 0 else None
+    hands = result.hand_landmarks if len(result.hand_landmarks) != 0 else None
     x = y = z = None
+    if hands is not None:
+        for hand in hands:
+            for i, point in enumerate(hand):
+                point = hand[i]
+                x = int(point.x * width)
+                y = int(point.y * height)
+                z = int(point.z)
+                cv2.circle(frame, (x,y), 2, (170, 0, 120), 2)
+# Line from Pointer finger to the Thumb
+            pointer = hand[8]
+            xP = int(pointer.x * width)
+            yP = int(pointer.y * height)
 
-    if hand is not None:
-        for i, point in enumerate(hand):
-            point = hand[i]
-            x = int(point.x * width)
-            y = int(point.y * height)
-            z = int(point.z)
-            cv2.circle(frame, (x,y), 2, (170, 0, 120), 2)
+            xPointer = pointer.x
+            yPointer = pointer.y
+            zPointer = abs(pointer.z)
+
+            thumb = hand[4]
+            xT = int(thumb.x * width)
+            yT = int(thumb.y * height)
+
+            distance = math.sqrt((xP - xT)  ** 2 + (yP - yT) ** 2)
+
+            cv2.line(frame, (xP, yP), (xT, yT), (255,255,255), 2)
+            cv2.putText(frame, f"{int(distance)} pixels", (int((xP + xT)/2) - 10, int((yP + yT)/2) - 10),cv2.FONT_HERSHEY_SIMPLEX, 1, ((xPointer * 255, yPointer * 255, zPointer * 255)))
+
+
+# Finished that
+
 
     cv2.imshow("Camera", frame)
 
